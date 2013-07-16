760a3951f1321bd5ccdf727ae9969981731399b1
sabudaye <cafekoma@gmail.com>
2013-07-16 10:28:31 +0400

    missing method 2

---
+++ classes/missing_method2.rb
@@ -0,0 +1,6 @@
+class MissingMethod2
+
+	def find_even(array)
+		array.find(&:even?)
+	end
+end
---
+++ tests/test_missing_method2.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "missing_method2"
+
+class TestMissingMethod2 < MiniTest::Unit::TestCase
+
+	def setup
+		@missing2 = MissingMethod2.new
+	end
+
+	def test_missing_method2
+		assert_equal @missing2.find_even([1, 7, 4, 9, 8]), 4
+		assert_equal @missing2.find_even([1, 3, 6, 10]), 6
+	end
+end
